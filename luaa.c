851a87bff1aaf48a32d29a92e75210e0a5904991
Jimmy Cozza <moatcozza@gmail.com>
2023-02-09 13:21:30 -0700

    move dwl here

---
+++ luaa.c
@@ -0,0 +1,10 @@
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+#include <xkbcommon/xkbcommon.h>
+
+void
+luaA_parserc(const char *rcfile)
+{
+  lua_State *L = luaL_newstate();
+}
---
+++ luaa.h
@@ -0,0 +1,5 @@
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+void luaA_parserc(const char *rcfile);
